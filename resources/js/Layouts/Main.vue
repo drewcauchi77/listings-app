a8a456065b739c5d99b0d9a77b57adf61e111994
Andrew Cauchi <drewcauchi@gmail.com>
2025-07-27 13:44:17 +0200

    logout

--- resources/js/Layouts/Main.vue
+++ resources/js/Layouts/Main.vue
@@ -1,16 +1,44 @@
 <script setup>
 import { switchTheme } from "@/theme.js";
 import NavLink from "@/Components/NavLink.vue";
+import {usePage} from "@inertiajs/vue3";
+import {computed, ref} from "vue";
+
+const page = usePage();
+const user = computed(() => page.props.auth.user);
+
+const show = ref(false);
 </script>
 
 <template>
+    <div v-show="show" @click="show = false" class="fixed inset-0 z-40"></div>
+
     <header class="bg-slate-800 text-white">
         <nav class="p-6 mx-auto max-w-screen-lg flex items-center justify-between">
             <NavLink routeName="home" componentName="Home">Home</NavLink>
 
             <div class="flex items-center space-x-6">
-                <NavLink routeName="login" componentName="Auth/Login">Login</NavLink>
-                <NavLink routeName="register" componentName="Auth/Register">Register</NavLink>
+                <div v-if="user" class="relative">
+                    <div
+                        @click="show = !show"
+                        class="flex items-center gap-2 px-3 py-1 rounded-lg hover:bg-slate-700 cursor-pointer"
+                        :class="{'bg-slate-700' : show}">
+                        <p>{{ user.name }}</p>
+                        <i class="fa-solid fa-angle-down"></i>
+                    </div>
+
+                    <div
+                        v-show="show"
+                        @click="show = false"
+                         class="absolute z-50 top-16 right-0 bg-slate-800 text-white rounded-lg border-slare-border overflow-hidden w-40">
+                        <Link class="block w-full px-6 py-3 hover:bg-slare-700 text-left">Dashboard</Link>
+                        <Link as="button" method="post" :href="route('logout')" class="block w-full px-6 py-3 hover:bg-slare-700 text-left">Logout</Link>
+                    </div>
+                </div>
+                <div v-else class="space-x-6">
+                    <NavLink routeName="login" componentName="Auth/Login">Login</NavLink>
+                    <NavLink routeName="register" componentName="Auth/Register">Register</NavLink>
+                </div>
 
                 <button @click="switchTheme()" class="hover:bg-slate-700 w-6 h-6 grid place-items-center rounded-full hover:outline outline-1 outline-white">
                     <i class="fa-solid fa-circle-half-stroke"></i>
--- routes/auth.php
+++ routes/auth.php
@@ -11,3 +11,7 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthenticateController::class, 'create'])->name('login');
     Route::post('/login', [AuthenticateController::class, 'store'])->name('login');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthenticateController::class, 'destroy'])->name('logout');
+});
